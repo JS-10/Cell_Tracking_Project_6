ece20869aa23bf8f0c1cd3d415f134ac996348f0
JS-10 <93281725+JS-10@users.noreply.github.com>
2025-07-13 19:05:37 +0200

    Small fix to model.py

--- deep_sort_pytorch/deep_sort/deep/model.py
+++ deep_sort_pytorch/deep_sort/deep/model.py
@@ -50,7 +50,7 @@ def make_layers(c_in, c_out, repeat_times, is_downsample=False):
 
 class Net(nn.Module):
     # Adaptation: Change number of classes to 3376 (matches with custom trained appearance model)
-    def __init__(self, num_classes=124, reid=False):
+    def __init__(self, num_classes=3376, reid=False):
         super(Net, self).__init__()
         # 3 128 64
         self.conv = nn.Sequential(
